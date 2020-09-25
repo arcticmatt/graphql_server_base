a6b109a23e46c2b4d4f632745cc179a0c8b40a40
Matt Lim <mlimcollege@gmail.com>
2020-09-24 22:49:42 -0700

    Add jest stuff to setup script and repo.

--- init_script.sh
+++ init_script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 yarn
-yarn add apollo-server graphql typescript
-yarn add -D eslint ts-node-dev @typescript-eslint/parser @typescript-eslint/eslint-plugin
+yarn add apollo-server graphql 
+yarn add -D eslint jest ts-jest ts-node-dev typescript 
+yarn add -D @types/jest @typescript-eslint/parser @typescript-eslint/eslint-plugin
+yarn ts-jest config:init
 mkdir src
 touch src/index.ts
 touch .gitignore
